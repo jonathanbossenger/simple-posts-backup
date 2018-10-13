55db923d106c2985456ccb21fd568df1a20559e8
Jonathan Bossenger <jonathanbossenger@gmail.com>
2018-10-13 20:15:38 +0200

    Initial commit, empty plugin shell

---
+++ simple-posts-backup.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Plugin Name: Simple Posts Backup
+ * Version: 1.0
+ * Plugin URI: http://jonathanbossenger.com/
+ * Description: Back up your post title and content.
+ * Author: Jonathan Bossenger
+ * Author URI: http://jonathanbossenger.com/
+ * Requires at least: 4.0
+ * Tested up to: 4.0
+ *
+ * Text Domain: simple-posts-backup
+ * Domain Path: /lang/
+ *
+ * @package WordPress
+ * @author Jonathan Bossenger
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// do your stuff heres
